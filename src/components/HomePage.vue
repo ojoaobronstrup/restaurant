192c15022fbdb18d1b88cfc45b802f2dd288c9ef
João Brönstrup <joaog.gob@gmail.com>
2024-06-06 21:09:14 -0300

    feat: static home page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,54 @@
+<template>
+  <header class="header">
+    <img src="./../assets/icon-restaurant.svg" alt="restaurant icon">
+    <img src="./../assets/icon-hamburguer.svg" alt="restaurant icon">
+  </header>
+  <body>
+    <div>
+      <h2>LISTA DE MESAS</h2>
+      <div>
+        <button @click="firstFloor" @blur="nothingSelected">Primeiro Piso</button>
+        <button @click="secondFloor" @blur="nothingSelected">Segundo Piso</button>
+      </div>
+    </div>
+  </body>
+  <footer>
+
+  </footer>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        floor: 0,
+      }
+    },
+
+    methods: {
+      firstFloor() {
+        this.floor = 1;
+        console.log(this.floor)
+      },
+      secondFloor() {
+        this.floor = 2;
+        console.log(this.floor)
+      },
+      nothingSelected() {
+        this.floor = 0;
+        console.log(this.floor)
+      }
+    }
+  }
+</script>
+
+<style>
+  .header{
+    width: 100vw;
+    height: 85px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    border-bottom: 1px solid lightgray;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import HomePage from './components/HomePage.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: HomePage },
+    { path: '/oi', component: <h1>oi</h1> }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+createApp(App).use(router).mount('#app')
